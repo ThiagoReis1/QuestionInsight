3651b463450bea23531a11312255ae1759b68dfa
ThiagoReis1 <thiago2003reis@gmail.com>
2025-08-18 14:10:22 -0400

    Update e Revisão Geral

--- Etapa_1/codebench-mining-tool/temp_code.py
+++ Etapa_1/codebench-mining-tool/temp_code.py
@@ -1,3 +1,9 @@
-nome= input("Insira seu nome aqui")
-
-print(nome.upper())
+from numpy import*
+pin = array(eval(input("")))
+pin_novo = zeros(size(pin), dtype = int)
+for i in range (size(pin)):
+	if pin[i]== 0:
+		pin_novo [i] = 9
+	else:
+		pin_novo[i] = pin[i]- 1
+print(pin_novo)
